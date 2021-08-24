bf8f84e99217c452f26db0832ad5e418c8b8252e
Damien M <67398532+GunFighterMan101@users.noreply.github.com>
2021-08-24 11:30:07 -0400

    Add files via upload

---
+++ TotallyFreeDiscordNitro_NotVirus.bat
@@ -0,0 +1,25 @@
+:: Use this to troll your friends
+@ECHO OFF 
+TITLE GET JEBAITED NERD
+ECHO Leaking System Information...
+ECHO ...
+systeminfo | findstr /c:"OS Name"
+systeminfo | findstr /c:"System Type"
+ipconfig | findstr IPv4
+ipconfig | findstr IPv6
+set url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+:: This is a link to Never Gonna Give You Up - Rick Astley
+start microsoft-edge:%url%
+start chrome %url%
+start firefox.exe %url%
+ECHO ============================
+ECHO WHAT JUST HAPPENED?
+ECHO ============================
+ECHO You just got rickrolled, retard.
+ECHO ============================
+ECHO ============================
+ECHO CREDIT
+ECHO ============================
+ECHO YOU GOT TROLLED BY GunFighterMan101 // Damien M
+ECHO Oh yeah... I also have your IP (thanks)
+PAUSE
